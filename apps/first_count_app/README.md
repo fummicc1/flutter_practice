32f8104ac5969393fb730bb47f244a870beb4230
fummicc1 <fumiya.tennis1@gmail.com>
2021-07-24 01:24:33 +0900

    First goal (Create normal and custom-designed count App)

--- apps/first_count_app/README.md
+++ apps/first_count_app/README.md
@@ -4,3 +4,15 @@
 
 - https://zenn.dev/kboy/books/ca6a9c93fd23f3/viewer/0f62d6#scaffold
 
+
+## やること
+
+1. 普通のカウントアプリ
+2. 背景色を変える（黒色）
+3. SafeAreaをつける
+4. TextStyleを変更する
+     - フォントサイズ
+     - フォントウェイト
+     - フォントカラー（白色）
+5. Columnを使って二つのカウントを表示する
+6. ListViewを使って複数のカウントを表示する
--- apps/first_count_app/lib/main.dart
+++ apps/first_count_app/lib/main.dart
@@ -12,7 +12,47 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(body: Container()),
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            child: Text(
+              "$count",
+              style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: plusNumber,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  // 配列の要素を一つ増やす
+  void plusNumber() {
+    setState(() {
+      count += 1;
+    });
   }
 }
